0d6178e92ce7b48baec54137d6c34293502a8365
Naman Maheshwari <nimcet07@gmail.com>
2024-08-12 00:00:22 +0530

    Update cors policy

--- src/main/java/com/development/newswebsite/config/CorsConfig.java
+++ src/main/java/com/development/newswebsite/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://bharatsamvad.netlify.app") // Ensure this is correct
+                        .allowedOrigins("http://localhost:3000","https://bharatsamvad.netlify.app") // Ensure this is correct
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
